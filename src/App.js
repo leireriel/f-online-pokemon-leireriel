63fc3125730eb9b11afe833f28c1bc0355eb2ff6
leireriel <leirericop@gmail.com>
2019-06-17 14:33:30 +0200

    Finish fetch and local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import { fetchPokemons } from './services/fetchPokemons';
 
 class App extends Component {
   constructor(props) {
@@ -6,32 +7,34 @@ class App extends Component {
     this.state = {
       pokemons: []
     };
+    this.getPokemons = this.getPokemons.bind(this);
   }
 
   componentDidMount() {
+    this.getPokemons();
+  }
+
+  getPokemons() {
     const getItem = JSON.parse(localStorage.getItem('pokemonsUser'));
     if (getItem === null) {
-      fetch('https://pokeapi.co/api/v2/pokemon?limit=25')
-      .then(res => res.json())
+      fetchPokemons()
       .then(data => {
         const pokeData = data.results;
         pokeData
-          .map((item, index) => {
-            item.id = index + 1;
-              return (
-              fetch(`https://pokeapi.co/api/v2/pokemon/${item.id}/`)
-              .then(res => res.json())
-              .then(data => {
-                const pokeDetails = data;
-                item.details = pokeDetails;
-                console.log(item)
-              })
+          .map(item => {
+            return (
+              fetch(item.url)
+                .then(res => res.json())
+                .then(data => {
+                  const newState = this.state.pokemons;
+                  newState.push(data);
+                  this.setState({
+                    pokemons: newState
+                  })
+                  localStorage.setItem('pokemonsUser', JSON.stringify(newState));
+                })
             );
           })
-        localStorage.setItem('pokemonsUser', JSON.stringify(pokeData));
-        this.setState({
-          pokemons: pokeData
-        })
       })
     } else {
       this.setState({
@@ -49,10 +52,22 @@ class App extends Component {
       {pokemons
         .map(item => {
           return (
-            <li key={item.id}>
-              
-              <h2>{item.name}</h2>
+            <li key={item.id - 1}>
+              <img 
+                src={item.sprites.front_default} 
+                alt={`${item.name} front`}
+              />
               <p>ID/{item.id}</p>
+              <h2>{item.name}</h2>
+              <ol>
+                {item.types
+                  .map((item, index) => {
+                    return (
+                      <li key={index}>{item.type.name}</li>
+                    );
+                  })
+                }
+              </ol>
             </li>
           );
         })
